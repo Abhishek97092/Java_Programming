db86a619738629282e34804d1e1d5bb97765cb2d
Abhishek97092 <abhishekabhi18a@gmail.com>
2025-03-26 22:56:52 +0530

    Add files via upload

---
+++ Arthmetic.java
@@ -0,0 +1,31 @@
+package Gnanmani.java;
+
+public class Arthmetic {
+    public static void main(String[] args) {
+//        Scanner input =new Scanner(System.in);
+//        System.out.println("welcome to arthmetic value");
+//        int num1;
+//        int num2;
+//        System.out.println("enter two number");
+//        num1=input.nextInt();
+//        num2=input.nextInt();
+//        System.out.println(num1+num2);
+//        System.out.println(num1-num2);
+//        System.out.println(num1*num2);
+//        System.out.println(num1/num2);
+//
+        int a =8;
+        int b =5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        double d=3;
+        double f=7;
+        System.out.println(d+f);
+        System.out.println(d-f);
+        System.out.println(d*f);
+        System.out.println(d/f);
+    }
+}
